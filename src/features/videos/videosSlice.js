1271a0a5ea37ee873583a0addc20347e8b006c62
Rasalahmad <rasalahmadsit@gmail.com>
2022-09-03 01:26:08 +0600

    wip

---
+++ src/features/videos/videosSlice.js
@@ -0,0 +1,39 @@
+const { createSlice, createAsyncThunk } = require("@reduxjs/toolkit");
+const { getVideos } = require("./videosAPI");
+
+// initial state
+const initialState = {
+  videos: [],
+  isLoading: false,
+  isError: false,
+  error: "",
+};
+
+const fetchVideos = createAsyncThunk("videos/fetchVideos", async () => {
+  const videos = await getVideos();
+  return videos;
+});
+
+const videoSlice = createSlice({
+  name: "videos",
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchVideos.pending, (state) => {
+        state.isLoading = false;
+        state.isError = false;
+      })
+      .addCase(fetchVideos.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.videos = action.payload;
+      })
+      .addCase(fetchVideos.rejected, (state, action) => {
+        state.isLoading = false;
+        state.videos = [];
+        state.isError = true;
+        state.error = action.error?.message;
+      });
+  },
+});
+
+export default videoSlice.reducer;
